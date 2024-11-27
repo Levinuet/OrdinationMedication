da3d3cc38c426732bcb3a274c3ecdf996ca6a4bc
Levi Emkjær <eaa23leej@students.eaaa.dk>
2024-11-27 11:14:02 +0100

    Oprettelse af tests

--- ordination-test/DagligFastTest.cs
+++ ordination-test/DagligFastTest.cs
@@ -1,10 +1,24 @@
 ﻿namespace ordination_test;
 
+using Service;
 using shared.Model;
+using Microsoft.EntityFrameworkCore;
+using Data;
 
 [TestClass]
 public class DagligFastTest
 {
+    private DataService service;
+
+    [TestInitialize]
+    public void SetupBeforeEachTest()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+        var context = new OrdinationContext(optionsBuilder.Options);
+        service = new DataService(context);
+        service.SeedData();
+    }
 
     [TestMethod]
     public void DagligDosisTest()
@@ -31,6 +45,67 @@ public class DagligFastTest
         Assert.AreEqual(forventetDoegnDosis, faktiskDoegnDosis, 0.0001, "DoegnDosis blev ikke beregnet korrekt");
     }
 
+    [TestMethod]
+    public void OpretDagligFast()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
 
+        Assert.AreEqual(1, service.GetDagligFaste().Count());
 
+        service.OpretDagligFast(patient.PatientId, lm.LaegemiddelId,
+            2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(2, service.GetDagligFaste().Count());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligFastPatientIdFindesIkke()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        service.OpretDagligFast(10, lm.LaegemiddelId, 2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig fast fejlet korrekt, da patient id ikke findes");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligFastLægemiddelIdFindesIkke()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        service.OpretDagligFast(patient.PatientId, 8, 2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig fast fejlet korrekt, da lægemiddel id ikke findes");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligFastPatientIdNegativ()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        service.OpretDagligFast(-1, lm.LaegemiddelId, 2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig fast fejlet korrekt, da patient id er negativ");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligFastLægemiddelIdNegativ()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        service.OpretDagligFast(patient.PatientId, -1, 2, 2, 1, 0, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig fast fejlet korrekt, da lægemiddel id er negativ");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligFaststartDatostørreendslutDato()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        service.OpretDagligFast(patient.PatientId, -1, 2, 2, 1, 0, DateTime.Now.AddDays(3), DateTime.Now);
+        Console.WriteLine("oprettelse af daglig fast fejlet korrekt, startDato kan ikke være større end slutDato");
+    }
 }
--- ordination-test/DagligSkævTest.cs
+++ ordination-test/DagligSkævTest.cs
@@ -1,9 +1,40 @@
 ﻿namespace ordination_test;
+
+using Data;
+using Microsoft.EntityFrameworkCore;
+using Service;
 using shared.Model;
 
 [TestClass]
 public class DagligSkævTest
 {
+
+    private DataService service;
+
+    [TestInitialize]
+    public void SetupBeforeEachTest()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrdinationContext>();
+        optionsBuilder.UseInMemoryDatabase(databaseName: "test-database");
+        var context = new OrdinationContext(optionsBuilder.Options);
+        service = new DataService(context);
+        service.SeedData();
+    }
+
+    [TestMethod]
+    public void OpretDagligSkaev()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+
+        Assert.AreEqual(1, service.GetDagligSkæve().Count());
+
+        Dosis[] doser = new Dosis[] { new Dosis(new DateTime(2024, 1, 1, 12, 0, 0), 2) };
+        service.OpretDagligSkaev(patient.PatientId, lm.LaegemiddelId, doser, DateTime.Now, DateTime.Now.AddDays(3));
+
+        Assert.AreEqual(2, service.GetDagligSkæve().Count());
+    }
+
     [TestMethod]
     public void doegnDosis()
     {
@@ -30,6 +61,63 @@ public class DagligSkævTest
         //Assert
 
         Assert.AreEqual(9, result);
+    }
 
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligSkævPatientIdFindesIkke()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        Dosis[] doser = new Dosis[] { new Dosis(new DateTime(2024, 1, 1, 12, 0, 0), 2) };
+        service.OpretDagligSkaev(10, lm.LaegemiddelId, doser, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig skæv fejlet korrekt, da patient id ikke findes");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligSkævLægemiddelIdFindesIkke()
+    {
+        Patient patient = service.GetPatienter().First();
+        Laegemiddel lm = service.GetLaegemidler().First();
+        Dosis[] doser = new Dosis[] { new Dosis(new DateTime(2024, 1, 1, 12, 0, 0), 2) };
+        service.OpretDagligSkaev(patient.PatientId, 10, doser, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig skæv fejlet korrekt, da lægemiddel id ikke findes");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligSkævPatientIdNegativ()
+    {
+        Dosis[] doser = new Dosis[] { new Dosis(new DateTime(2024, 1, 1, 12, 0, 0), 2) };
+        service.OpretDagligSkaev(-1, 1, doser, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig skæv fejlet korrekt, da patient id er negativ");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligSkævLægemiddelIdNegativ()
+    {
+        Dosis[] doser = new Dosis[] { new Dosis(new DateTime(2024, 1, 1, 12, 0, 0), 2) };
+        service.OpretDagligSkaev(1, -1, doser, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig skæv fejlet korrekt, da lægemiddel id er negativ");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligSkævDoserTom()
+    {
+        Dosis[] doser = new Dosis[] { };
+        service.OpretDagligSkaev(1, 1, doser, DateTime.Now, DateTime.Now.AddDays(3));
+        Console.WriteLine("oprettelse af daglig skæv fejlet korrekt, da doser er tom");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void OpretDagligSkaevstartDatostørreendslutDato()
+    {
+        Dosis[] doser = new Dosis[] { new Dosis(new DateTime(2024, 1, 1, 12, 0, 0), 2) };
+        service.OpretDagligSkaev(1, -1, doser, DateTime.Now.AddDays(3), DateTime.Now);
+        Console.WriteLine("oprettelse af daglig skæv fejlet korrekt, startDato kan ikke være større end slutDato");
     }
 }
